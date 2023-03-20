b1a380494e72013182f2288ea698e5a856115cdf
champimeli <114486882+champimeli@users.noreply.github.com>
2023-03-20 11:04:12 +0100

    ejercicios clases
    insanos

---
+++ javi(no borrar)/saludo.js
@@ -0,0 +1,11 @@
+//ejercicio 2
+
+//definicion de la funcion
+let nombre = "josefa"
+function saludo(nombre){
+    return "hellow"  + nombre + ",¿como estas?";
+}
+
+var mensaje = saluda("josefa");
+console.log(mensaje)
+
