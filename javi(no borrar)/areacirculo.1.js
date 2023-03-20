baf0d0d17f1884954def5aaf1f52dd09cc62faad
champimeli <114486882+champimeli@users.noreply.github.com>
2023-03-20 11:10:45 +0100

    EJERCICIOS FUNCIONES

--- javi(no borrar)/areacirculo.1.js
+++ javi(no borrar)/areacirculo.1.js
@@ -1,1 +1,9 @@
-function areacirculo();
+function area(radio){
+    let total = radio *radio *math.PI;
+return total;
+
+}
+
+total_area = area(3);
+
+console.log("el area de radiop 3 es:" + total_area)
